5d9e12f11c6ee0f5d4cb1e071cecd647aec307ee
MohammadRazaChoudhary <choudharym08@hamkids.co.uk>
2016-07-05 17:07:44 +0800

    Update main2.js

--- main2.js
+++ main2.js
@@ -3,16 +3,19 @@ var mainState = {
                 game.load.image('grass', 'grass.png');
                 game.load.image('plant', 'plant.png');
                 game.load.image('background', 'background.png');
-                game.load.atlasJSONHash('zombie', 'sprites.png' , 'sprites.json');
+                game.load.atlasJSONHash('sprites', 'sprites.png' , 'sprites.json');
     },
 
     create: function() { 
     // game.stage.backgroundColor = '#71c5cf';
     game.add.image(0,0, 'grass');
-    sprite = game.add.sprite(10, 10, 'zombie', 'zombie1.png');
+    sprite = game.add.sprite(10, 10, 'sprites', "zombie1.png");
     plant = game.add.image(0, 300, 'plant');
     game.add.image(0, 0, 'background');
     gameOver = game.add.text(180, 20, "MNE Creations", {font: "10px Arial", fill: "#ffffff"});
+
+    game.plant = game.add.physicsGroup(Phaser.Physics.ARCADE, game.world, 'plant');
+    Phaser.ScaleManager = this (this, 1000,1450);
 
     },
 
@@ -26,7 +29,7 @@ var mainState = {
 
 };
 
-var game = new Phaser.Game(300,450);
+var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO, 'gameArea');
 
 game.state.add('main', mainState); 
 
