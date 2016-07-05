f126d4681829930c3589bef26d4552289226b06d
MohammadRazaChoudhary <choudharym08@hamkids.co.uk>
2016-07-05 15:12:55 +0800

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,7 +1,7 @@
 var mainState = {
     preload: function() { 
                 game.load.image('grass', 'assets/grass.png');
-                game.load.image('plant', 'plant.png');
+                game.load.image('plant', 'assets/plant.png');
                 game.load.image('background', 'assets/background.png');
                 game.load.atlasJSONHash('zombie', 'assets/sprites.png' , 'assets/sprites.json');
     },
@@ -12,7 +12,7 @@ var mainState = {
     sprite = game.add.sprite(10, 10, 'zombie', 'zombie1.png');
     plant = game.add.image(0, 300, 'plant');
     game.add.image(0, 0, 'background');
-    gameOver = game.add.text(180, 20, "MNE Creations", {font: "30px Arial", fill: "#ffffff"});
+    gameOver = game.add.text(180, 20, "MNE Creations", {font: "10px Arial", fill: "#ffffff"});
 
     },
 
@@ -26,7 +26,7 @@ var mainState = {
 
 };
 
-var game = new Phaser.Game(300, 450);
+var game = new Phaser.Game(300,450);
 
 game.state.add('main', mainState); 
 
