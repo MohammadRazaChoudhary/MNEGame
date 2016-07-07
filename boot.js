fe50837320ac3646db3bf84399e710968a65556f
MohammadRazaChoudhary <choudharym08@hamkids.co.uk>
2016-07-07 14:53:26 +0800

    Update boot.js

--- boot.js
+++ boot.js
@@ -18,7 +18,7 @@ var bootState = {
   create: function() {
     this.game.CELL_WIDTH = 490 / 12;
     this.game.CELL_HEIGHT = 290 / 7;
-    game.add.image(0, 0, 'background');
+    game.add.image(0, 0, 'grass');
     return this.game.state.start('Title');
   }
 };
