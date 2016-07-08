6e7a6bb8ea276ad5dc8250b7df93f972011356b7
MohammadRazaChoudhary <choudharym08@hamkids.co.uk>
2016-07-08 10:57:23 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var Game, game;
+
+window.game = game = new Phaser.Game(300, 450, Phaser.AUTO, '');
+
+game.state.add('Boot', bootState);
+
+game.state.add('Title', titleState);
+
+game.state.add('Play', playState);
+
+game.state.add('Over', overState);
+
+game.state.start('Boot');
