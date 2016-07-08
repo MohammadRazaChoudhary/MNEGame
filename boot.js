51124e5579be7ce4764efb9c0b76ecd60f494a8d
MohammadRazaChoudhary <choudharym08@hamkids.co.uk>
2016-07-08 12:54:53 +0800

    Update boot.js

--- boot.js
+++ boot.js
@@ -38,30 +38,56 @@
 
 // game.state.start('boot'); 
 
-var bootState = {
-    preload: function() { 
-                        this.game.load.atlasJSONHash('sprites', 'sprites.png' , 'sprites.json');
-                this.game.load.image('grass', 'grass.png');
-                this.game.load.image('plant', 'plant.png');
-                this.game.load.image('background', 'background.png');
+var TranslationDefender = {};
 
+TranslationDefender.boot = function(game) {};
+
+TranslationDefender.Boot.prototype = {
+    preload: function() {
+        
     },
     
-    create: function() { 
-    game.stage.backgroundColor = '#71c5cf';
-    this.game.CELL_WIDTH = 490 / 12;
-    this.game.CELL_HEIGHT = 290 / 7;
-    this.game.add.image(0,0, 'grass');
-    this.sprite = game.add.sprite(10, 10, 'sprites', "zombie1.png");
-    plant = game.add.image(0, 300, 'plant');
-    this.game.add.image(0, 0, 'background');
+    create: function() {
+        this.input.maxPointers = 1;
+        this.stage.disableVisibilityChange = false;
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.minWidth = 300;
+        this.scale.minHeight = 450;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        this.scale.forcePortrait = true;
+        this.scale.setScreenSize = true;
+        
+    }
+}
+// var bootState = {
+//     preload: function() { 
+//                         this.game.load.atlasJSONHash('sprites', 'sprites.png' , 'sprites.json');
+//                 this.game.load.image('grass', 'grass.png');
+//                 this.game.load.image('plant', 'plant.png');
+//                 this.game.load.image('background', 'background.png');
 
-    game.plant = game.add.physicsGroup(Phaser.Physics.ARCADE, game.world, 'plant');
-    Phaser.ScaleManager = this (this, 1000,1450);
-    return this.game.state.start('Title');
+//     },
+    
+//     create: function() { 
+
+        
+    // game.stage.backgroundColor = '#71c5cf';
+    // this.game.CELL_WIDTH = 490 / 12;
+    // this.game.CELL_HEIGHT = 290 / 7;
+    // this.game.add.image(0,0, 'grass');
+    // this.sprite = game.add.sprite(10, 10, 'sprites', "zombie1.png");
+    // plant = game.add.image(0, 300, 'plant');
+    // this.game.add.image(0, 0, 'background');
+
+    // game.plant = game.add.physicsGroup(Phaser.Physics.ARCADE, game.world, 'plant');
+    // Phaser.ScaleManager = this (this, 1000,1450);
+    // return this.game.state.start('Title');
+    
+    
     
 
-    },
+    // },
 
     update: function() {
 
