1c2d0e51b0b6ad5e764e3a18701d0062c5df9f9a
MohammadRazaChoudhary <choudharym08@hamkids.co.uk>
2016-07-08 10:41:48 +0800

    Update boot.js

--- boot.js
+++ boot.js
@@ -47,6 +47,13 @@ var bootState = {
 
     },
     
+      init: function() {
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.minWidth = 490;
+    this.game.scale.minHeight = 290;
+    this.game.scale.maxWidth = 980;
+    this.game.scale.maxHeight = 580;
+      },
 
     create: function() { 
     game.stage.backgroundColor = '#71c5cf';
