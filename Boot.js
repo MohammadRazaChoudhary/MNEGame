f4d5d7b576a354d72a0e9695902d3be6d88483be
MohammadRazaChoudhary <choudharym08@hamkids.co.uk>
2016-07-08 15:25:54 +0800

    Update Boot.js

--- Boot.js
+++ Boot.js
@@ -1,61 +1,78 @@
-BasicGame.Preloader = function (game) {
+BasicGame = {
 
-	this.background = null;
-	this.preloadBar = null;
+    /* Here we've just got some global level vars that persist regardless of State swaps */
+    score: 0,
 
-	this.ready = false;
+    /* If the music in your game needs to play through-out a few State swaps, then you could reference it here */
+    music: null,
+
+    /* Your game can check BasicGame.orientated in internal loops to know if it should pause or not */
+    orientated: false
 
 };
 
-BasicGame.Preloader.prototype = {
+BasicGame.Boot = function (game) {
+};
 
-	preload: function () {
+BasicGame.Boot.prototype = {
 
-		//	These are the assets we loaded in Boot.js
-		//	A nice sparkly background and a loading progress bar
-		this.background = this.add.sprite(0, 0, 'preloaderBackground');
-		this.preloadBar = this.add.sprite(100, 100, 'preloaderBar');
-		
-		//	This sets the preloadBar sprite as a loader sprite.
-		//	What that does is automatically crop the sprite from 0 to full-width
-		//	as the files below are loaded in.
-		this.load.setPreloadSprite(this.preloadBar);
+    init: function () {
 
-		//	Here we load the rest of the assets our game needs.
-		//	As this is just a Project Template I've not provided these assets, the lines below won't work as the files themselves will 404, they are just an example of use.
-		this.load.image('titlepage', 'images/title.jpg');
-		this.load.atlas('playButton', 'images/play_button.png', 'images/play_button.json');
-		this.load.audio('titleMusic', ['audio/main_menu.mp3']);
-		this.load.bitmapFont('caslon', 'fonts/caslon.png', 'fonts/caslon.xml');
-		//	+ lots of other required assets here
+        this.input.maxPointers = 1;
+        this.stage.disableVisibilityChange = true;
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.setMinMax(480, 260, 1024, 768);
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
 
-	},
+        if (!this.game.device.desktop)
+        {
+            this.scale.forceOrientation(true, false);
+            this.scale.setResizeCallback(this.gameResized, this);
+            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+        }
 
-	create: function () {
-		gameOver = game.add.text(180, 20, "MNE Creations", {font: "10px Arial", fill: "#ffffff"});
+    },
 
-		//	Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
-		this.preloadBar.cropEnabled = false;
+    preload: function () {
 
-	},
+        //  Here we load the assets required for our preloader 
+        this.load.image('grassBackground', 'grassBackground.png')
+        this.load.image('preloaderBackground', 'preloaderBackground.jpg');
+        this.load.image('preloaderBar', 'preloaderBar.png');
+        this.load.image('plant', 'plant.png');
 
-	update: function () {
+    },
 
-		//	You don't actually need to do this, but I find it gives a much smoother game experience.
-		//	Basically it will wait for our audio file to be decoded before proceeding to the MainMenu.
-		//	You can jump right into the menu if you want and still play the music, but you'll have a few
-		//	seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu
-		//	it's best to wait for it to decode here first, then carry on.
-		
-		//	If you don't have any music in your game then put the game.state.start line into the create function and delete
-		//	the update function completely.
-		
-		if (this.cache.isSoundDecoded('titleMusic') && this.ready == false)
-		{
-			this.ready = true;
-			this.state.start('MainMenu');
-		}
+    create: function () {
 
-	}
+        this.state.start('Preloader');
+
+    },
+
+    gameResized: function (width, height) {
+
+        //  This could be handy if you need to do any extra processing if the game resizes.
+        //  A resize could happen if for example swapping orientation on a device or resizing the browser window.
+        //  Note that this callback is only really useful if you use a ScaleMode of RESIZE and place it inside your main game state.
+
+    },
+
+    enterIncorrectOrientation: function () {
+
+        BasicGame.orientated = false;
+
+        document.getElementById('orientation').style.display = 'block';
+
+    },
+
+    leaveIncorrectOrientation: function () {
+
+        BasicGame.orientated = true;
+
+        document.getElementById('orientation').style.display = 'none';
+
+    }
 
 };
