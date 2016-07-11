0619097982340768deb72ef9a54e13f3eab80168
MohammadRazaChoudhary <choudharym08@hamkids.co.uk>
2016-07-11 10:36:58 +0800

    Update Preloader.js

--- Preloader.js
+++ Preloader.js
@@ -32,13 +32,6 @@ BasicGame.Preloader.prototype = {
 
 	},
 
-	create: function () {
-
-		//	Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
-		this.preloadBar.cropEnabled = false;
-
-	},
-
 	update: function () {
 
 		var text;
@@ -46,21 +39,13 @@ BasicGame.Preloader.prototype = {
 			font: '80px fonts',
 			fill: '#FFF'
 		});
-		//	You don't actually need to do this, but I find it gives a much smoother game experience.
-		//	Basically it will wait for our audio file to be decoded before proceeding to the MainMenu.
-		//	You can jump right into the menu if you want and still play the music, but you'll have a few
-		//	seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu
-		//	it's best to wait for it to decode here first, then carry on.
-		
-		//	If you don't have any music in your game then put the game.state.start line into the create function and delete
-		//	the update function completely.
-		
-		if (this.cache.isSoundDecoded('titleMusic') && this.ready == false)
-		{
-			this.ready = true;
-			this.state.start('MainMenu');
-		}
 
-	}
-
+    text.anchor.setTo(0.5, 0.5);
+    return this.game.input.onTap.add((function(_this) {
+      return function() {
+        return _this.game.state.start('Play');
+      };
+    })(this));
+  }
 };
+
